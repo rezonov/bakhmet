9b425276858922dd67fc0b4ba56002fdbf6692bd
rezonov <e@rezonov.ru>
2018-02-05 15:47:39 +0300

    Новое всплывающее меню

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -164,7 +164,19 @@
     }
 
     $('.dropdown-toggle').dropdown();
-
+    $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $(this).parent().addClass('open');
+        var menu = $(this).parent().find("ul");
+        var menupos = menu.offset();
+        if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+            var newpos = - menu.width();
+        } else {
+            var newpos = $(this).parent().width();
+        }
+        menu.css({ left:newpos });
+    });
 </script>
 
 </body>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,16 +3,34 @@
         @foreach($menu as $m)
             @if($m->level == 0)
                 <li>{{$m->name}}
-                <ul>
+                <ul class="submenu">
                     <li><a href=#>Sudmenu 1</a></li>
                     <li><a href=#>Sudmenu 1</a></li>
                     <li><a href=#>Sudmenu 1</a></li>
-                </ul></li>
+                </ul>
+                </li>
 
             @endif
 
             @endforeach
-        <li>Насосы</li>
+            <li class="menu-item dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Навигация <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Меню</a></li>
+                    <li><a href="#">Меню</a></li>
+                    <li class="menu-item dropdown dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Меню</a>
+                        <ul class="dropdown-menu">
+                            <li class="menu-item "><a href="#">Подменю</a></li>
+                            <li class="menu-item "><a href="#">Подменю</a></li>
+                            <li class="menu-item dropdown dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Подменю</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Подменю в квадрате</a></li>
+                                    <li><a href="#">Подменю в квадрате</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
         <li>Насосы</li>
         <li>Насосы</li>
         <li>Насосы</li>
