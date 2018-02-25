9b6bad41a509c4b7a586db80fda25b83e52a73cd
rezonov <e@rezonov.ru>
2018-02-25 17:20:01 +0300

    Форма

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -38,8 +38,8 @@ class SettingsController extends Controller
 
         foreach ($request->all() as $key => $item) {
           if($key !== '_token') {
-            /*  $Set = Settings::firstOrNew(array("name" => $key, "value" => $item ));
-              $Set->save();*/
+             $Set = Settings::firstOrNew(array("name" => $key, "value" => $item ));
+              $Set->save();
             dump($key);
           }
 
