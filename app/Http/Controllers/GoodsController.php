f4d5c2a40505d5459e2341a6a7969aaea0b0db41
rezonov <e@rezonov.ru>
2018-03-02 09:56:36 +0300

    Slick

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -693,7 +693,7 @@ class GoodsController extends Controller
             ->join('catalog as CG', 'CG.parent', '=', 'C.id')
             ->select('C.id', 'C.name', 'C.parent', 'C.latin_name', 'CG.latin_name as LN')
             ->get();
-        dump($Allc);
+
         foreach ($Allc as $AC) {
             if($AC->parent != '0') {
                 DB::table('catalog')
