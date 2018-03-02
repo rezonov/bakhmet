a61bd517f9fd11a75c684a0e99e57ffd5b3a905a
rezonov <e@rezonov.ru>
2018-03-02 20:05:23 +0300

    Slick

--- resources/views/searchpage.blade.php
+++ resources/views/searchpage.blade.php
@@ -10,22 +10,6 @@
 
 @stop
 
-@section('keywords')
-    @if( (!empty($Seo->keywords)))
-        $Seo->keywords
-    @else
-        "ТД Бахмет"
-    @endif
-
-@stop
-@section('descriptions')
-    @if( (!empty($Seo->keywords)))
-        $Seo->keywords
-    @else
-        "ТД Бахмет"
-    @endif
-
-@stop
 
 @section ('content)
 
