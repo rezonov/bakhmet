76f6e0c2c54f2f57a25691f4f36acf577779b1c8
rezonov <e@rezonov.ru>
2018-03-02 16:48:00 +0300

    Slick

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -14,7 +14,7 @@
                 @if($header[$i]['Sh'] != 'Off')
                     <td>
                         @if ($i==1)
-                            <a href="/{{$Url[$tr[0]]}}">{{$tr[$i]}}</a>
+                            <a href="/{{$catalog}}/{{$Url[$tr[0]]}}">{{$tr[$i]}}</a>
                             <a onClick="if($('#descr{{$tr[0]}}').css('display') == 'none') { $('#descr{{$tr[0]}}').css('display', 'block') } else { $('#descr{{$tr[0]}}').css('display', 'none') }">{{$tr[$i]}}</a>
                         @elseif(!empty($tr[$i]))
                             {{$tr[$i]}}
