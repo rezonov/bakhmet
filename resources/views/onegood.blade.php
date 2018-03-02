0c6bca98f4d8b3899eea4bd2a6949c36f2c5f8ca
rezonov <e@rezonov.ru>
2018-03-02 19:05:36 +0300

    Slick

--- resources/views/onegood.blade.php
+++ resources/views/onegood.blade.php
@@ -4,6 +4,8 @@
 
 
 @section ('content')
+    <div class="col-md-3"></div>
+    <div class="col-md-9">
     <table class="table">
 
 
@@ -14,4 +16,5 @@
             </tr>
     @endforeach
     </table>
+    </div>
 @stop
