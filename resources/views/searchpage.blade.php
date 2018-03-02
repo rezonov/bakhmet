d6a7d01b1820261c4c35e987ba9c74553663c093
rezonov <e@rezonov.ru>
2018-03-02 21:33:20 +0300

    Slick

--- resources/views/searchpage.blade.php
+++ resources/views/searchpage.blade.php
@@ -4,10 +4,12 @@
 
 
 @section ('content')
-
+<h2>Результаты поиска:</h2>
     <table class="table">
         @foreach($names as $n)
         <tr>
+            <td><a href="/good/{{$n->Clat}}/{{$n->Name}}">{{$n->Gname}}</a></td>
+            <td>{{$n->Gname}}</td>
             <td>{{$n->Name}}</td>
         </tr>
             @endforeach
