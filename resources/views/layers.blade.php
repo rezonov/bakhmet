cd7d1ec3c0ecabc3ff3d9fa71fc00131ed2dd8ea
rezonov <e@rezonov.ru>
2018-03-03 20:46:52 +0300

    Slick

--- resources/views/layers.blade.php
+++ resources/views/layers.blade.php
@@ -36,12 +36,14 @@
                 </div>
     <script>
         function filterTable($table, $col, $min, $max) {
+                <?php  $c = $c + 1;?>
                 var $Cols = [];
                 @foreach($attr as $tr)
                         @if ($tr['Fl'] != "Off")
-                    $Cols[{{$tr['id']}}] = $( "#amount-{{$tr['id']}}" ).val().split(' - ');
-                    @endif
+                            $Cols[{{$tr['id']}}] = $( "#amount-{{$tr['id']}}" ).val().split(' - ');
+                        @endif
                 @endforeach
+
                 var $Tols = [];
 
             console.log($Cols);
