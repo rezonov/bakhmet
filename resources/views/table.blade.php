4ae0e8c90dc7813c0d8c94fdb7f483e0a6305454
rezonov <e@rezonov.ru>
2018-03-05 12:00:41 +0300

    Slick

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -33,11 +33,11 @@
                 </button>
             </td>
         </tr>
-        <tr>
+        <tr id="descr{{$tr[0]}}">
             <td colspan="{{count($tr)}}">
 
                 @if(!empty($descr[$tr[0]]['text']))
-                    <div class="descrtd" id="descr{{$tr[0]}}" style="display: none;width:300px ">
+                    <div class="descrtd"  style="display: none;width:300px ">
                         <div style="float:left">
                             <img width="300" float="right" src="/img/{{ $descs[$tr[0]]['file'] }}"/>
                             @if(!empty($files[$tr[0]]))
