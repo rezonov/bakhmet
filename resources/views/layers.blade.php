4748c7484a5abcf56980036569574bc08038236e
rezonov <e@rezonov.ru>
2018-03-05 10:46:21 +0300

    Slick

--- resources/views/layers.blade.php
+++ resources/views/layers.blade.php
@@ -60,7 +60,7 @@
                 var valid = true;
 
 
-                    console.log(ind + $(this).text() + Cols[ind][0]);
+                    console.log(ind + $(this).text() + $Cols[ind][0]);
 
 
 
