414e0c71dc1bc85facdba412e128564f5b0007bd
rezonov <e@rezonov.ru>
2018-03-13 11:19:20 +0300

    Slick

--- resources/views/onegood.blade.php
+++ resources/views/onegood.blade.php
@@ -91,7 +91,7 @@
             <button type="button" class="btn btn-orange" data-toggle="modal" data-target="#myModal">
                 ЗАКАЗАТЬ
             </button>
-            <button type="button" class="btn btn-green" >
+            <button type="button" class="btn btn-green" class="inbasket">
                 В КОРЗИНУ
             </button>
         </div>
@@ -101,4 +101,10 @@
             </div>
         </div>
     </div>
+
+    <script>
+        $('.inbasket').click( function() {
+           alert("here");
+        });
+    </script>
 @stop
